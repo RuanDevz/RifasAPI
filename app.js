c6e6cb9e185a68dd81231acfae36296142fe8ef6
RuanDevz <Ruanbatista1509@outlook.com>
2024-08-02 16:59:31 -0300

    test

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const express = require("express");
 const cors = require("cors");
 const bodyParser = require("body-parser");
 const db = require("./models");
+const sendConfirmationEmail = require("./services/EmailSend");
 const pg = require("pg");
 
 
@@ -101,7 +102,60 @@ app.post("/create-checkout", async (req, res) => {
   res.send({ url: session.url });
 });
 
+app.post("/reduce-ticket", async (req, res) => {
+  const ticketsDisponiveis = await getTicketsDisponiveis();
+  const { quantity, email, name } = req.body;
 
+  if (!email || !name) {
+    return res.status(400).json({ error: "Email e nome são obrigatórios." });
+  }
+
+  if (ticketsDisponiveis >= quantity) {
+    const newTicketsDisponiveis = ticketsDisponiveis - quantity;
+    await updateTicketsDisponiveis(newTicketsDisponiveis);
+
+    // Geração dos tickets
+    const tickets = [];
+    for (let i = 0; i < quantity; i++) {
+      let ticketNumber;
+      let ticketExists = true;
+
+      while (ticketExists) {
+        ticketNumber = Math.floor(Math.random() * 1000000) + 1;
+        const existingTicket = await db.Ticket.findOne({
+          where: { ticket: ticketNumber },
+        });
+        if (!existingTicket) {
+          ticketExists = false;
+        }
+      }
+
+      const newTicket = await db.Ticket.create({
+        name,
+        email,
+        ticket: ticketNumber,
+        quantity: 1,
+      });
+
+      tickets.push(newTicket);
+    }
+
+    res.json({
+      message: "Ticket(s) reduzido(s) com sucesso",
+      ticketsDisponiveis: newTicketsDisponiveis,
+      tickets,
+    });
+
+    try {
+      await sendConfirmationEmail(email, name, tickets);
+    } catch (error) {
+      console.error("Error sending confirmation email:", error);
+      res.status(500).json({ error: "Erro ao enviar email de confirmação." });
+    }
+  } else {
+    res.status(400).json({ error: "Não há tickets suficientes disponíveis." });
+  }
+});
 
 app.post("/generate-tickets", async (req, res) => {
   const { name, email, quantity } = req.body;
