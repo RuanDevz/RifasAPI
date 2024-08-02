646d2ad11bee777ff01d652a1fce2ca839a66815
RuanDevz <Ruanbatista1509@outlook.com>
2024-08-02 11:53:18 -0300

    test cors

--- app.js
+++ app.js
@@ -14,9 +14,12 @@ app.use(express.json());
 app.use(bodyParser.raw({ type: "application/json" }));
 
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*'); 
+  res.header('Access-Control-Allow-Origin', 'https://sevenxvip.com');
   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
   res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  if (req.method === 'OPTIONS') {
+    return res.sendStatus(200); // Responde ao preflight OPTIONS
+  }
   next();
 });
 
