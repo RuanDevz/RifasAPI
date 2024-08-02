7250df185a06c80d8601ac3f0729383d628df04b
RuanDevz <Ruanbatista1509@outlook.com>
2024-08-02 11:48:34 -0300

    fix error cors

--- app.js
+++ app.js
@@ -13,6 +13,13 @@ app.use(cors());
 app.use(express.json());
 app.use(bodyParser.raw({ type: "application/json" }));
 
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*'); 
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  next();
+});
+
 const getTicketsDisponiveis = async () => {
   const result = await db.AvailableTickets.findOne({ where: {}, order: [['createdAt', 'DESC']] });
   return result ? result.tickets : 20000; 
